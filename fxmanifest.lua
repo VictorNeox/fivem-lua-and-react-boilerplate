07f9ed9f9f8bc8eb3d27fa886cd4253635d42570
Victor Neox <victorrodrigues753@gmail.com>
2021-07-09 22:10:36 -0300

    Boilerplate

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version "cerulean"
+
+description "Template para uso de React & Lua FiveM"
+author "Project Error"
+version '1.0.0'
+repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
+
+lua54 'yes'
+
+games {
+  "gta5",
+}
+
+ui_page 'web/build/index.html'
+
+client_script "client/**/*"
+server_script "server/**/*"
+
+files {
+  'web/build/index.html',
+  'web/build/**/*'
+}
